46fde73738efa62447bc08e5d23c748d8fa0bc5c
Ravneet Singh <ravneetsingh@Ravneets-MacBook-Pro.local>
2026-07-02 11:27:32 +0530

    Practice regression_line

--- BestFitSlop.py
+++ BestFitSlop.py
@@ -1,6 +1,9 @@
 from statistics import mean
+from matplotlib import style
 import numpy as np
 import matplotlib.pyplot as plt
+
+style.use('fivethirtyeight')
 
 xs = np.array([1,2,3,4,5,6],dtype=np.float64)
 ys = np.array([5,4,6,5,6,7],dtype=np.float64)
@@ -8,12 +11,23 @@ ys = np.array([5,4,6,5,6,7],dtype=np.float64)
 def best_fit_slope_and_intercept(xs,ys):
     m = ( ((mean(xs)*mean(ys)) - mean(xs*ys)) /
           ((mean(xs)*mean(xs)) - mean(xs*xs)) )
-    # b = mean(ys) - m*mean(xs)
-    # return m, b
-    return m
+    b = mean(ys) - m*mean(xs)
+    return m, b
+    # return m
 
-# m = best_fit_slope_and_intercept(xs,ys)[0]
-m = best_fit_slope_and_intercept(xs,ys)
-print(m)
+m, b = best_fit_slope_and_intercept(xs,ys)
+# b = best_fit_slope_and_intercept(xs,ys)[1]
+print(m,b)
+regression_line = [(m*x)+b for x in xs]
+
+predict_x = 8
+predict_y = (m*predict_x)+b
+
+
+plt.scatter(xs,ys)
+plt.scatter(predict_x,predict_y,color='g')
+plt.plot(xs, regression_line)
+plt.show()
+
 # plt.plot(xs, ys)
 # plt.show()
