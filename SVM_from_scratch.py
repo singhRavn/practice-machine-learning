98848cdf5eff8fa652258e3f87486ae6ba96dff9
Ravneet Singh <ravneetsingh@Ravneets-MacBook-Pro.local>
2026-07-22 18:28:06 +0530

    SvM started

--- SVM_from_scratch.py
+++ SVM_from_scratch.py
@@ -2,3 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import style
 style.use('ggplot')
+
+class Support_Vector_Machine:
+    def __init__(self, visualization=True):
+        self.visualization = visualization
+        self.colors = {1: 'r', -1: 'b'}
+        if self.visualization:
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(1, 1, 1)
+
+    def fit(self, data):
+        pass
+    def predict(self, features):
+        classification = np.sign(np.dot(np.array(features), self.w) + self.b)
